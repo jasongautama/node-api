074eced16cc00b23a03de3e6930b65b507aaf35d
Ken Prayogo <ken.prayogo@zomia.org>
2018-06-16 14:08:11 -0700

    CORS setup

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const dbHandler = require('./lib/db-handler');
 
 app.use(passport.initialize());
 
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'GET');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+	if ('OPTIONS' == req.method) {
+		 res.send(200);
+	 } else {
+		 next();
+	 }
+});
+
 passport.use(new Strategy(
 
 	/**
