2676d680f54f43366b44415069e036f240918a42
Ken Prayogo <ken.prayogo@zomia.org>
2018-06-16 14:15:53 -0700

    cors again

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(function(req, res, next) {
 	res.header('Access-Control-Allow-Credentials', true);
 	res.header('Access-Control-Allow-Origin', "*");
 	res.header('Access-Control-Allow-Methods', "GET,OPTIONS");
-	res.header('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+	res.header('Access-Control-Allow-Headers', "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 	if ('OPTIONS' == req.method) {
 		 res.send(200);
 	 } else {
