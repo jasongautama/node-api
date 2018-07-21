cd0736884e2ad19d5d65ed89e11c30c1cca81eff
Ken Prayogo <ken.prayogo@zomia.org>
2018-07-21 15:47:42 -0700

    Add StartMonth field and a virtual StartMonthYear field.

--- src/db/models/sermon-series.js
+++ src/db/models/sermon-series.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = require('../db-config');
 const DbModel = require('./db-model');
 const config = require('../../../config.json');
+const moment = require('moment');
 
 class SermonSeries extends DbModel {
 
@@ -11,8 +12,11 @@ class SermonSeries extends DbModel {
             ID: { type: Sequelize.INTEGER, primaryKey: true },
             Name: Sequelize.STRING(100),
             Year: Sequelize.INTEGER(4),
+            StartMonth: Sequelize.TINYINT(),
             ImagePath: Sequelize.STRING(255),
-            BannerImagePath: Sequelize.STRING(255)
+            BannerImagePath: Sequelize.STRING(255),
+            // Custom Columns
+            StartMonthYear: Sequelize.VIRTUAL
         }, { tableName: 'SermonSeries' });
 
         super(tableModel);
@@ -24,8 +28,12 @@ class SermonSeries extends DbModel {
      */
     processRows(rows) {
         for (const i in rows) {
-            rows[i].ImagePath = config.s3.prefix + rows[i].ImagePath;
-            rows[i].BannerImagePath = config.s3.prefix + rows[i].BannerImagePath;
+            // Add prefix to image paths
+            rows[i].ImagePath = rows[i].ImagePath ? config.s3.prefix + rows[i].ImagePath : '';
+            rows[i].BannerImagePath = rows[i].BannerImagePath ? config.s3.prefix + rows[i].BannerImagePath : '';
+            // Add a Month/Year string for nicer display
+            var date = moment(`${rows[i].Year}-${rows[i].StartMonth}-01`);
+            rows[i].StartMonthYear = date.format('MMMM YYYY'); 
         }
         return Promise.resolve(rows);
     }
